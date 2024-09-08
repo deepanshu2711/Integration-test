08e86d120213a9942f143a7a4fe0243ac15b4ee9
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-09-08 13:17:41 +0530

    added ci pipeline

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -1,8 +1,26 @@
-import { describe, expect, it } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { app } from "..";
 import request from "supertest";
+import resetDb from "./helpers/reset-db";
 
 describe("POST /sum", () => {
+    // if you want a piece of code to run before each test, you can use the `beforeEach` 
+    //before each it-block run 
+
+    // beforeEach(async() =>{
+    //     console.log("clearing the database")
+    //     await resetDb();
+    // })
+
+    // if you want a piece of code to run before all tests, you can use the `beforeAll` 
+    //before all it-blocks run just run this once
+    beforeAll(async() =>{
+        console.log("clearing the database")
+        await resetDb();
+    })
+
+
+
     it("should sum add 2 numbers", async () => {
         const { status, body } = await request(app).post('/sum').send({
             a: 1,
